b3ecd7afc8e01947302e5193a1302711e2594599
Eric-cha1 <eric@teamwise.dooray.com>
2026-03-12 16:59:49 +0900

    add: react-native expo 사용해서 기초 구조 생성 feat: fonts(Pretendard) 간단하게 적용
    todo: 폰트 상세 설정 및 다른 모든 설정 필요함

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,11 @@
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { Stack } from 'expo-router';
+import { SplashScreen, Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import 'react-native-reanimated';
 
 import { useColorScheme } from '@/hooks/use-color-scheme';
+import { useFonts } from 'expo-font';
+import { useEffect } from 'react';
 
 export const unstable_settings = {
   anchor: '(tabs)',
@@ -11,6 +13,19 @@ export const unstable_settings = {
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
+
+  const [loaded] = useFonts({
+    Pretendard: require('../assets/fonts/Pretendard-Regular.ttf'),
+    PretendardBold: require('../assets/fonts/Pretendard-Bold.ttf'),
+    PretendardSemiBold: require('../assets/fonts/Pretendard-SemiBold.ttf'),
+    PretendardMedium: require('../assets/fonts/Pretendard-Medium.ttf'),
+  });
+
+  useEffect(() => {
+    if (loaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded]);
 
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
